0aa563282eeb7df3f43da16e3d85bf6ea3a72823
Mateus Oliveira Preste <mateusPreste@users.noreply.github.com>
2017-05-18 11:50:36 -0400

    Update LU.py

--- Testing/LU.py
+++ Testing/LU.py
@@ -1,22 +1,69 @@
+from matplotlib.pyplot import *
 import numpy as np
+ 
+yp=np.array([139620, 173706, 311622])
+xp=np.array([1950, 1960, 1970]) 
 
-def LU(A):
-    U = np.zeros(A.shape)
-    
-    for i in range (0, A.shape[0]):
-        pivot = A[i][i]
-        U[i][i] = 1
-        
-        for j in range (i+1, A.shape[0]):
-            m = A[j][i]/pivot
-            U[j][i] = m
-            for k in range (i, A.shape[1]):
-                A[j][k] = A[j][k] - (m* A[i][k])
-    
-    return A,U
+ypo=np.array([311622, 633392, 1011500])
+xpo=np.array([1970, 1980, 1991])     
 
-uu = np.array( [ [1.,6,2,4,8],
-                [3,19,4,15,25],
-                [1,4,8,-12,18],
-                [5,33,9,3,72] ])
-print(LU(uu))
+ypq=np.array([139620, 173706, 311622, 633392])
+xpq=np.array([1950, 1960, 1970, 1980])   
+
+ypoq=np.array([311622, 633392, 1011500, 1403793])
+xpoq=np.array([1970, 1980, 1991, 2000])     
+    
+def polinear(M):
+    matx = np.zeros((M.shape[0], M.shape[0]))
+    for i in range (0, M.shape[0]):
+        for k in range(0, M.shape[0]):
+            matx[i][k] = pow(M[i],M.shape[0]-(k+1))
+    return matx
+
+def f(t, h, x):
+    total = 0
+
+    for i in range(0, x):
+        asf = x-(i+1)
+        total = total + h[i]*(t**(x-(i+1)))
+    return total
+    
+def interpol(z, x, y):
+    AH = np.squeeze(np.asarray(np.linalg.solve(polinear(x), y)))
+    return f(z, np.squeeze(np.asarray(AH)), AH.shape[0])
+    
+def lagrange(z, x, y):
+    n = x.shape[0] #size
+    L = 0 # value
+    for i in range(0, n):
+        num = 1
+        den = 1
+        for j in range(0,n):
+            if(j != i):
+                num = num * (z-x[j])
+                den = den * (x[i] - x[j])
+        L = L + y[i] * (num/den)
+    return L
+    
+print(interpol(1958, xp, yp))
+print(lagrange(1958, xp, yp))
+print(interpol(1988, xpo, ypo))
+print(lagrange(1988, xpo, ypo))
+
+print("")
+
+print(interpol(1958, xpq, ypq))
+print(lagrange(1958, xpq, ypq))
+print(interpol(1988, xpoq, ypoq))
+print(lagrange(1988, xpoq, ypoq))
+
+    
+t = np.linspace(1950, 1970, 2000)
+y = np.zeros(len(t))
+
+for i in range(len(t)):
+    y[i] = interpol(t[i], xp, yp)
+plot(t, y)
+
+
+show()
